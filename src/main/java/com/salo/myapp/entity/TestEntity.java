4f9ab1f28b4d2772e061de7588aea957a0a5f0f0
Salomon <13030564@itcelaya.edu.mx>
2021-11-11 16:41:58 -0600

    Nombre del Proyecto: my-app, Descripcion: verison final de my-app

---
+++ src/main/java/com/salo/myapp/entity/TestEntity.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.salo.myapp.entity;
+
+/**
+ * @author SaloCE
+ *
+ */
+public class TestEntity {
+	/*
+	 * Propiedad de prueba para la muestra de compilacion del archivo java
+	 * */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
